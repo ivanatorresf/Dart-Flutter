5be12a89a4949f36c61f72621e41b16681dfebf9
Adrian Torres <tec.adriantf@gmail.com>
2020-09-18 09:29:01 -0700

    exercises

---
+++ exercise5.dart
@@ -0,0 +1,54 @@
+void main(){
+
+  List<int> numeros =[1,2,3,4,5,6];
+  print(numeros);
+  // en <int> puede ser tambien listas de strings, double, int
+  numeros.add(7);
+  print(numeros);
+  
+  numeros.add(8);
+  print(numeros);
+  
+  numeros.add(9);
+  print(numeros);
+  
+  numeros.add(10);
+  print(numeros);
+  
+  
+  // tamaños fijo
+  List masNumeros = List (10);
+  print(masNumeros);
+  
+  masNumeros[0] = 1;
+  print(masNumeros);
+
+  masNumeros[1] = 2;
+  print(masNumeros);
+  
+  masNumeros[2] = 3;
+  print(masNumeros);
+  
+  masNumeros[3] = 4;
+  print(masNumeros);
+  
+  masNumeros[4] = 5;
+  print(masNumeros);
+  
+  masNumeros[5] = 6;
+  print(masNumeros);
+  
+  masNumeros[6] = 7;
+  print(masNumeros);
+  
+  masNumeros[7] = 8;
+  print(masNumeros);
+  
+  masNumeros[8] = 9;
+  print(masNumeros);
+  
+  masNumeros[9] = 10;
+  print(masNumeros);
+  
+  
+}
