0b577beff9c4551f5485281da7b0a69214c2b42d
Adrian Torres <tec.adriantf@gmail.com>
2020-09-23 10:50:49 -0700

    await async

---
+++ exercise 14 await async.dart
@@ -0,0 +1,18 @@
+void main() {
+
+    print('Estamos a punto de pedir datos');
+
+    String data = await httpGet('https://api.nasa.com/aliens');
+
+    print( data );
+
+    print('Ulitma linea');
+}
+
+
+Future<String>httpGet(String url) {
+
+  return Future.delayed( new Duration( seconds: 4), (){
+    return 'Hola Mundo';
+  });
+}
