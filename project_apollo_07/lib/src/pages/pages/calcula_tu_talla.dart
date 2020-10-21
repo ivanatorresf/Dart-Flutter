eca58072e80ea2af97e8a9603548e55547d1d8dd
Ivan Torres <tec.adriantf@gmail.com>
2020-10-21 16:52:32 -0700

    Update

--- project_apollo_07/lib/src/pages/pages/calcula_tu_talla.dart
+++ project_apollo_07/lib/src/pages/pages/calcula_tu_talla.dart
@@ -20,21 +20,61 @@ class MainApp extends StatelessWidget {
             body: TabBarView(
               children: <Widget>[
                 /*Text("FirstTExt")*/
+
                 Card(
                   child: Column(
-                    children: <Widget>[
+                    children: [
                       Image(
                           image: NetworkImage(
                               'https://cdn.shopify.com/s/files/1/0300/5926/6141/products/C2_1024x1024.png?v=1596070441'))
                     ],
                   ),
                 ),
-                Text("SecondTExt"),
-                Text("ThirdTExt"),
-                Text("FdTExt"),
+
+                /*Text("SecondTExt"),*/
+                Card(
+                  child: Column(
+                    children: [
+                      Image(
+                          image: NetworkImage(
+                              'https://cdn.shopify.com/s/files/1/0300/5926/6141/products/JW1_1024x1024.png?v=1594415388'))
+                    ],
+                  ),
+                ),
+                /*Text("ThirdTExt"),*/
+                Card(
+                  child: Column(
+                    children: [
+                      Image(
+                          image: NetworkImage(
+                              'https://cdn.shopify.com/s/files/1/0300/5926/6141/products/lateral-frontal_902093fa-f7fe-444f-be7c-37820cfab930_280x.png?v=1600883673'))
+                    ],
+                  ),
+                ),
+                /*Text("FdTExt"),*/
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      _crearSlider()
+                      /* Image(
+                          image: NetworkImage(
+                              'https://cdn.shopify.com/s/files/1/0300/5926/6141/products/BODYINVISIBLE1_280x.jpg?v=1590621733'))
+                   */
+                    ],
+                  ),
+                ),
               ],
             ),
           )),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+      value: 100,
+      min: 10.0,
+      max: 400.0,
+      onChanged: (valor) {},
     );
   }
 }
